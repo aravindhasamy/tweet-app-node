f056764ee5038d0f7b5c7c4aabf277c76a75d913
aravindhasamy <aravindhasamy9@gmail.com>
2021-09-21 02:38:15 +0530

    Added console.log

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 var cors = require('cors')
+console.log("before mongoose")
 require('./db/mongoose')
+console.log("after mongoose")
 const tweetRouter = require('./routers/tweet')
+console.log("router initialized")
 
 const app = express()
+console.log("after app")
 app.use(express.json())
 app.options('*', cors())
 app.use(tweetRouter)
-
+console.log("end of app")
 
 
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+console.log("before index app")
 const app = require('./app')
+console.log("after index app")
 // const port = process.env.PORT || 3005
 console.log("process.env.PORT ", process.env.PORT )
 app.listen(process.env.PORT || 3005, '0.0.0.0', () => {
