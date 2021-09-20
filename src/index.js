8154173a14a9df6bf0fd010d34e90e0fba843a4f
aravindhasamy <aravindhasamy9@gmail.com>
2021-09-21 02:26:50 +0530

    added console.log for process.env.port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
-const port = process.env.PORT || 3005
-app.listen(port, '0.0.0.0', () => {
-    console.log('Server is up on port ' + port)
+// const port = process.env.PORT || 3005
+console.log("process.env.PORT ", process.env.PORT )
+app.listen(process.env.PORT || 3005, () => {
+    console.log('Server is up on port ' + process.env.PORT )
 })
