b2d7ffb0ac10ef189dec7d0993542b5002a3fb48
aravindhasamy <aravindhasamy9@gmail.com>
2021-09-21 02:20:24 +0530

    added dummy host name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 const port = process.env.PORT || 3005
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log('Server is up on port ' + port)
 })
